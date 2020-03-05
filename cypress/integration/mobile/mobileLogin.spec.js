44a9518ae71a08f8ae9aaa11e7f4129e21598c75
Mans <mans@finder.com>
2020-03-05 14:49:20 +1100

    test(automation): lint checkup

--- cypress/integration/mobile/mobileLogin.spec.js
+++ cypress/integration/mobile/mobileLogin.spec.js
@@ -1,15 +1,15 @@
 describe("Validate user login", function() {
-  this.beforeEach(() => {
-      cy.visit('/user-profile/sign-in');
-      cy.viewport(375, 812)
-      //open browser in iphoneX viewport
-  })
-  it('should display error message when login form is submit with empty fields', () => {
-      cy.clickContinueButton();
-      cy.get('[date-testid="VALIDATION_ERROR_MESSAGE"]').should('be.visible');
-  });
-  it('should not allow to login with invalid login details', () => {
-      cy.invalidLoginInputs();
-      cy.get('.luna-form__alert--error').should('be.visible')
-  });
+    this.beforeEach(() => {
+        cy.visit('/user-profile/sign-in');
+        cy.viewport(375, 812);
+        //open browser in iphoneX viewport
+    });
+    it('should display error message when login form is submit with empty fields', () => {
+        cy.clickContinueButton();
+        cy.get('[date-testid="VALIDATION_ERROR_MESSAGE"]').should('be.visible');
+    });
+    it('should not allow to login with invalid login details', () => {
+        cy.invalidLoginInputs();
+        cy.get('.luna-form__alert--error').should('be.visible');
+    });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,8 +3,8 @@ Cypress.Commands.add('clickContinueButton', () => {
 });
 
 Cypress.Commands.add('invalidLoginInputs', () => {
-    cy.get('[name="email_or_mobile_number"]').type('123@test.com')
+    cy.get('[name="email_or_mobile_number"]').type('123@test.com');
     cy.clickContinueButton();
-    cy.get('[name="user_pass"]').type('test1234')
+    cy.get('[name="user_pass"]').type('test1234');
     cy.clickContinueButton();
 });
